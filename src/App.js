9503c0731d1f15c7fa81b610aeac40c60235a41c
Savannah <71588465+SavanCode@users.noreply.github.com>
2022-05-04 14:35:09 +0800

    feat: simple frame

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import "./style.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import ErrorPage from "./Pages/ErrorPage";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/profile"> Profile </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <div> Foooter </div>
+    </Router>
   );
 }
+
+export default App;
